991b57fb71bedcf1d67bb51b02570c00da1703d1
Alumno <Alumno@Windows7-PC>
2018-02-21 12:49:32 +0100

    Version 1.0.0 Bienvenido GitHub

---
+++ testgit/src/testgit/Testgit.java
@@ -0,0 +1,11 @@
+
+package testgit;
+
+public class Testgit {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello GitHub");
+    }
+    
+}
