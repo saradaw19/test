3ca19a4011b52065108375f0d96188c2adca323a
saradaw19 <36697548+saradaw19@users.noreply.github.com>
2018-02-21 13:04:03 +0100

    Update Testgit.java

--- testgit/src/testgit/Testgit.java
+++ testgit/src/testgit/Testgit.java
@@ -6,7 +6,6 @@ public class Testgit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello GitHub");
-        System.out.println("Estoy en el GitHub");
     }
     
 }
